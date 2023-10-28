2d0afba33a6ab8a83af0576298edb6dfc471bcbf
Priyadarshini N M <priyadarshininm.ec21@bmsce.ac.in>
2023-10-28 20:04:31 +0530

    updating file

---
+++ MAC.v
@@ -0,0 +1,24 @@
+module MAC (
+  input clk, reset,
+  input [31:0] in1, in2,
+  output reg [31:0] sum_high, sum_low
+);
+
+reg [63:0] sum = 0;
+
+always @(posedge clk or posedge reset)
+  begin
+    if (reset)
+      begin
+        sum_high <= 32'b0;
+        sum_low <= 32'b0;
+      end
+    else
+      begin
+        sum = sum + (in1 * in2);
+        sum_high <= sum[63:32];
+        sum_low <= sum[31:0];
+    end
+end
+
+endmodule
