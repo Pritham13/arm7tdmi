b87a25933142219735a5d4e04642c5112d62872f
Pritham13 <prithamhsuvarna@gmail.com>
2023-10-29 23:57:36 +0530

    updating files

--- MAC/MAC.v
+++ MAC/MAC.v
@@ -1,16 +1,12 @@
 module MAC (
   input clk,
   input [31:0] in1, in2, acc,
-  output reg [31:0] sum_high, sum_low
+  output reg [31:0] sum
 );
-
-reg [63:0] sum = 0;
 
 always @(posedge clk )
   begin
     sum = acc + (in1 * in2);
-    sum_high <= sum[63:32];
-    sum_low <= sum[31:0];
   end
 
 endmodule
