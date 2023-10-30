23360fbdbdf018fadade79b021216c475bf13394
Pritham13 <prithamhsuvarna@gmail.com>
2023-10-30 15:03:02 +0530

    updating files

--- MAC/MAC_tb.v
+++ MAC/MAC_tb.v
@@ -2,5 +2,23 @@ module MAC_tb ();
 reg [31:0] in_a,in_b,acc;
 wire [31:0] out;
 MAC(.in1(in_a),.in2(in_b),.acc(acc),.sum(out))
-    
+
+initial 
+    begin
+        for (integer i=0;i<10;i=i+1)
+            begin
+                in_a = i;
+                in_b = i;
+                out = 0;
+                #100
+            end
+            #100
+            for (integer i=0;i<10;i=i+1)
+            begin
+                in_a = i;
+                in_b = i;
+                out = 1'd1;
+                #100
+            end
+    end
 endmodule
