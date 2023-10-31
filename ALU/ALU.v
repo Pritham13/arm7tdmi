5de52f3fcc7b11564815bf76895f950e26e9dced
Pritham13 <prithamhsuvarna@gmail.com>
2023-10-31 21:19:39 +0530

    updating files

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,71 +1,75 @@
-/*
-ALU Control lines | Function
------------------------------
-        0000    Bitwise-AND
-        0001    Bitwise-OR
-        0010	Add (A+B)
-        0100	Subtract (A-B)
-        0101	Set on less than
-        0110    Multiply
-        0111    Bitwise-XOR
-        */
-//////////////////////////////////////
-/////Pritham:control values in case statement needs to be changed/////
-//cpsr flags needs to be added (carry,overflow,negative)/////////
-///////////////////////////////////////////
+`define ADD 4'b0000  // 0b0000 represents addition
+`define SUB 4'b0001  // 0b0001 represents subtraction
+`define ADDS 4'b0010  // 0b0010 represents add with carry flag
+`define SUBS 4'b0011  // 0b0011 represents subtraction with negative flag
+`define CMP 4'b0100  // 0b0100 represents comparison
+`define AND 4'b0111  // 0b0111 represents bitwise AND
+`define OR  4'b1000  // 0b1000 represents bitwise OR
+`define XOR 4'b1001  // 0b1001 represents bitwise XOR
+`define MVN 4'b1010  // 0b1010 represents bitwise NOT
+
 module ALU(
     input [31:0] operand_a, operand_b,
     input [3:0] alu_control,
     output reg [31:0] result,
-    output reg zero_flag
+    output reg zero_flag,
+    output reg carry_flag,
+    output reg overflow_flag,
+    output reg negative_flag
 );
     always @(*)
         begin
              case(alu_control)
-                4'b0000: result = operand_a&operand_b;
-                4'b0001: result = operand_a|operand_b;
-                4'b0010: result = operand_a+operand_b;
-                4'b0011: result = operand_a-operand_b;
-                4'b0101: result = operand_a^operand_b;
-                //not operations have to be checked in case only one input is recieved how will it be processed
-                4'b0101: result = ~operand_a;
-                4'b0101: result = ~operand_b;
-                //less than or greater than
-                4'b0100: 
+                `AND: 
+                    result = operand_a & operand_b;
+                `ADDS: 
+                    overflow_detect(
+                        .in1(operand_a),
+                        .in2(operand_b),
+                        .result(result),
+                        .overflow(overflow_flag)
+                    );
                     begin
-                        if(operand_a<operand_b)
+                           zero_flag = (result == 32'd0) ? 1'b1 : 1'b0;
+                    end
+                `OR: 
+                    result = operand_a | operand_b;
+                `ADD: 
+                    result = operand_a + operand_b;
+                `SUB: 
+                    result = operand_a - operand_b;
+                `XOR: 
+                    result = operand_a ^ operand_b;
+                // Handle the NOT operation
+                `MVN: 
+                    result = ~operand_a;
+                // Handle the comparison operation
+                `CMP: 
+                    begin
+                        if (operand_a < operand_b)
                             result = 1'b1;
                         else
                             result = 1'b0;
                     end
-                //inequality 
-                4'b0101: 
-                    begin
-                        if(operand_a==operand_b)
-                            result = 1'b1;
-                        else
-                            result = 1'b0;
-                    end
-                //less than or eqaual
-                4'b0101: 
-                    begin
-                        if(operand_a<=operand_b)
-                            result = 1'b1;
-                        else
-                            result = 1'b0;
-                    end
-                //greater than or equal
-                4'b0101: 
-                    begin
-                        if(operand_a>=operand_b)
-                            result = 1'b1;
-                        else
-                            result = 1'b0;
-                    end
+                   
             endcase
-            if(result ==0)
-                zero_flag = 1'b1;
-            else    
-                zero_flag = 1'b0;
         end
+endmodule
+
+module overflow_detect (
+    input signed [31:0] in1,
+    input signed [31:0] in2,
+    output signed [31:0] result,
+    output overflow
+);
+
+    wire signed [32:0] in2_w;  // Extend 1 bit for saturation
+
+    assign in2_w = in2 + in1;  // Signed arithmetic
+
+    assign result = (in2_w[32:31] == 2'b01) ? {1'b0, {(32-1){1'b1}}}
+                                           : (in2_w[32:31] == 2'b10) ? {1'b1, {(32-1){1'b0}}}
+                                           : in2_w[31:0];
+
+    assign overflow = (in2_w[32:31] == 2'b01 || in2_w[32:31] == 2'b10);
 endmodule
--- ALU/ALU_tb.v
+++ ALU/ALU_tb.v
@@ -1,27 +1,45 @@
-//leaving it as it is as on 29-10-23 for further improvements and discussion 
-module ALU_tb();
-    reg [31:0] a,b;
-    reg [3:0] ALU_control;
-    wire ZERO_flag;
-    wire [31:0] Result;
+module ALU_Testbench;
 
-    ALU alu_module(.operand_a(a),.operand_b(b),.alu_control(ALU_control),.zero_flag(ZERO_flag),.result(Result));
+    reg [31:0] operand_a;
+    reg [31:0] operand_b;
+    reg [3:0] alu_control;
+    
+    wire [31:0] result;
+    wire zero_flag;
+    wire carry_flag;
+    wire overflow_flag;
+    wire negative_flag;
 
+    // Instantiate the ALU module
+    ALU uut (
+        .operand_a(operand_a),
+        .operand_b(operand_b),
+        .alu_control(alu_control),
+        .result(result),
+        .zero_flag(zero_flag),
+        .carry_flag(carry_flag),
+        .overflow_flag(overflow_flag),
+        .negative_flag(negative_flag)
+    );
 
-    initial
-        begin
-            $dumpfile("alu_output.vcd");
-            $dumpvars(0,ALU_tb);
-        end
-    initial
-        begin
-            a = 23; b = 42;  ALU_control = 4'b0000;
-        #20 a = 23; b = 42;  ALU_control = 4'b0001;
-        #20 a = 23; b = 42;  ALU_control = 4'b0010;
-        #20 a = 23; b = 42;  ALU_control = 4'b0011;
-        #20 a = 23; b = 42;  ALU_control = 4'b0100;
-        #20 a = 42; b = 23;  ALU_control = 4'b0110;
-        end
-    initial
-        #150 $finish;
+    initial begin
+        // Initialize test inputs
+        operand_a = 32'hA5A5A5A5;
+        operand_b = 32'h5A5A5A5A;
+        alu_control = `ADD; // You can change this to test different operations
+        
+        // Wait for some time
+        #10;
+
+        // Display results
+        $display("ALU Result: %h", result);
+        $display("Zero Flag: %b", zero_flag);
+        $display("Carry Flag: %b", carry_flag);
+        $display("Overflow Flag: %b", overflow_flag);
+        $display("Negative Flag: %b", negative_flag);
+
+        // End the simulation
+        $finish;
+    end
+
 endmodule
