7e692ee74ef3fe8afe76493f5e6424fb2f390756
Priyadarshini N M <priyadarshininm.ec21@bmsce.ac.in>
2023-11-20 12:29:36 +0530

    Update testbench

--- Register/Register_tb.v
+++ Register/Register_tb.v
@@ -1,65 +1,73 @@
-module register_test;
+`timescale 1ns/1ns
 
-reg[3:0] read_address;
-reg[3:0] write_address;
+module registers_tb;
 
-reg[31:0] write_data;
-reg write_enable = 0;
+  // Parameters
+  parameter CLOCK_PERIOD = 10;
 
-reg[31:0] pc_update;
-reg pc_write;
+  // Signals
+  reg [31:0] read_reg_num1, read_reg_num2, write_reg, write_data;
+  reg zero_flag, carry_flag, overflow_flag, negative_flag, regwrite;
+  reg clock, reset;
 
-reg clk;
+  // Instantiate the registers module
+  registers uut (
+    .read_reg_num1(read_reg_num1),
+    .read_reg_num2(read_reg_num2),
+    .write_reg(write_reg),
+    .zero_flag(zero_flag),
+    .carry_flag(carry_flag),
+    .overflow_flag(overflow_flag),
+    .negative_flag(negative_flag),
+    .write_data(write_data),
+    .read_data1(),
+    .read_data2(),
+    .regwrite(regwrite),
+    .clock(clock),
+    .reset(reset)
+  );
 
-wire[31:0] out_data_1;
-wire[31:0] cspr;
-wire[31:0] pc;
+  // Clock generation
+  always begin
+    #CLOCK_PERIOD / 2 clock = ~clock;
+  end
 
-register_file RegisterFile(	//inputs
-							.in_address1(read_address),
-							.in_address2(),
-							.in_address3(),
-							.in_address4(write_address),
+  // Initial block
+  initial begin
+    // Initialize inputs
+    read_reg_num1 = 0;
+    read_reg_num2 = 1;
+    write_reg = 2;
+    write_data = 32'h12345678;
+    zero_flag = 0;
+    carry_flag = 0;
+    overflow_flag = 0;
+    negative_flag = 0;
+    regwrite = 1;
+    clock = 0;
+    reset = 1;
 
-							.universal_read_address(),
-							
-							.in_data(write_data),
-							.write_enable(write_enable),
-							.pc_update(pc_update), 
-							.pc_write(pc_write), 
-							.cspr_write(), 
-							.cspr_update(),
-							.clk(clk), 
-							
-							//outputs
-							.out_data1(out_data_1),
-							.out_data2(),
-							.out_data3(),
-							
-							.universal_out_data(),
-							
-							.pc(pc), 
-							.cspr(cspr)			
-							
-							);
+    // Apply reset
+    #CLOCK_PERIOD reset = 0;
 
-initial begin
-	#1 clk = 1;
+    // Test case 1: Write data to register
+    #CLOCK_PERIOD;
+    assert(uut.read_data1 === 32'h0) else $fatal("Test case 1 failed");
+    assert(uut.read_data2 === 32'h0) else $fatal("Test case 1 failed");
+    assert(uut.CPSR === 4'b0000) else $fatal("Test case 1 failed");
 
-	#2 clk = 0;
+    // Test case 2: Write data to register and check read data
+    write_reg = 0;
+    write_data = 32'hABCDEF01;
+    #CLOCK_PERIOD;
+    assert(uut.read_data1 === 32'hABCDEF01) else $fatal("Test case 2 failed");
+    assert(uut.read_data2 === 32'h0) else $fatal("Test case 2 failed");
+    assert(uut.CPSR === 4'b0000) else $fatal("Test case 2 failed");
 
-	#3 clk = 1;
-	read_address = 0;
-	
-	#4 clk = 0;
-	$display("pc- %h" , pc);
+    // Add more test cases as needed
 
-	#5 clk = 1;
-	pc_update = pc+4;
-	pc_write = 1;
-
-	#6 clk = 0;
-	$display("pc %h", pc);
-end
+    $display("All test cases passed!");
+    $stop;
+  end
 
 endmodule
