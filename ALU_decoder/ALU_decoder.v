1b4d3884ee1a77f876cbf8e44d00240e813594a9
Pritham13 <prithamhsuvarna@gmail.com>
2023-12-09 12:20:46 +0530

    updating ALU_decode

--- ALU_decoder/ALU_decoder.v
+++ ALU_decoder/ALU_decoder.v
@@ -1,5 +1,14 @@
 module ALU_decoder (
-    input opcode
+    input [3:0]opcode,
+    input reset,
+    output reg alu_control
 );
-    
+always @(posedge reset)begin
+    alu_control = 1'b0;
+end
+always @(opcode)begin
+    case opcode
+        4'b0:alu_control = 4'd0;//ADD
+    endcase
+end
 endmodule
