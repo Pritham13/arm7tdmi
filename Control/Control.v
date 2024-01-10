43100974c9a50052619e37068303b6db6febc26c
Pritham13 <prithamhsuvarna@gmail.com>
2024-01-11 00:03:29 +0530

    updating control.v

--- Control/Control.v
+++ Control/Control.v
@@ -1,5 +1,15 @@
 module Control (
-    input
+    input [31:0]instruction,
+    output reg [31:0]ALUop,multiply_op,
+    output reg [23:0]Branch_op
 );
+always @ (*)//to be changed later
+begin
+    case (instruction[27:25])
+        3'b001:ALUop = instruction;
+        3'b101:Branch_op = instruction[23:0];
+        3'b000:multiply_op = instruction;
+    endcase
     
+end
 endmodule
